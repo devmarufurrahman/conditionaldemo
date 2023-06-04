9b416ee19e0ea13c184f72e3b61bd5e209a414fb
marufur-rahman <mdmarufur5682@gmail.com>
2023-06-04 14:20:30 +0600

    condition error fix

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import Route from "../Route";
 const Header = () => {
 	const [component, setComponent] = useState("");
 	const handleNavigate = (e) => {
-		setComponent(e.target.innerText);
+		setComponent(e);
 	};
 
 	return (
@@ -46,7 +46,9 @@ const Header = () => {
 						<ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 							<li>
 								<a
-									onClick={handleNavigate}
+									onClick={() => {
+										handleNavigate("Home");
+									}}
 									href="#"
 									className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
 									aria-current="page">
@@ -55,7 +57,9 @@ const Header = () => {
 							</li>
 							<li>
 								<a
-									onClick={handleNavigate}
+									onClick={() => {
+										handleNavigate("About");
+									}}
 									href="#"
 									className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
 									About
@@ -63,7 +67,9 @@ const Header = () => {
 							</li>
 							<li>
 								<a
-									onClick={handleNavigate}
+									onClick={() => {
+										handleNavigate("Services");
+									}}
 									href="#"
 									className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
 									Services
@@ -71,7 +77,9 @@ const Header = () => {
 							</li>
 							<li>
 								<a
-									onClick={handleNavigate}
+									onClick={() => {
+										handleNavigate("Pricing");
+									}}
 									href="#"
 									className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
 									Pricing
@@ -79,7 +87,9 @@ const Header = () => {
 							</li>
 							<li>
 								<a
-									onClick={handleNavigate}
+									onClick={() => {
+										handleNavigate("Contact");
+									}}
 									href="#"
 									className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
 									Contact
